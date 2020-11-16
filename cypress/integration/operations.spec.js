2b4ad90c7e7ce0fcc44c9d47209559189ab76929
lfnandoo <devfernandoof@gmail.com>
2020-11-15 23:04:26 -0300

    test: "should create naver card" test created

--- cypress/integration/operations.spec.js
+++ cypress/integration/operations.spec.js
@@ -4,13 +4,13 @@ describe("Operations", () => {
   it("should have logged in", () => {
     cy.visit("https://navers-test-lfnandoo.vercel.app/");
 
-    cy.get("[data-cy=email").type("testing-user@nave.rs");
-    cy.get("[data-cy=password").type("nave1234");
+    cy.get("[data-cy=email]").type("testing-user@nave.rs");
+    cy.get("[data-cy=password]").type("nave1234");
 
     cy.server();
     cy.route("POST", "**/v1/users/login").as("postLogin");
 
-    cy.get("[data-cy=submit").click();
+    cy.get("[data-cy=submit]").click();
 
     cy.wait("@postLogin").then(({ status, response }) => {
       expect(status).be.equal(200);
@@ -18,9 +18,46 @@ describe("Operations", () => {
       expect(response.body).has.property("token");
       expect(response.body.id).is.not.null;
       expect(response.body.token).is.not.null;
+      expect(localStorage.getItem("@Navers:token")).be.equal(
+        response.body.token
+      );
+      expect(localStorage.getItem("@Navers:id")).be.equal(response.body.id);
+    });
+  });
 
-      Cypress.env("userLoginToken", response.body.token);
-      Cypress.env("userLoginToken");
+  it("should create naver card", () => {
+    cy.get('[data-cy="create-naver"]').click();
+
+    cy.get('[data-cy="name"]')
+      .type("Fernando")
+      .should("have.value", "Fernando");
+    cy.get('[data-cy="job_role"]')
+      .type("Frontend Developer")
+      .should("have.value", "Frontend Developer");
+    cy.get('[data-cy="birthdate"]')
+      .type("2003-10-07")
+      .should("have.value", "2003-10-07");
+    cy.get('[data-cy="admission_date"]')
+      .type("2015-07-05")
+      .should("have.value", "2015-07-05");
+    cy.get('[data-cy="project"]')
+      .type("Naver Test")
+      .should("have.value", "Naver Test");
+    cy.get('[data-cy="url"]')
+      .type("https://google.com")
+      .should("have.value", "https://google.com");
+
+    cy.server();
+    cy.route("POST", "**/navers").as("postNewNaverCard");
+
+    cy.get("[data-cy=submit]").click();
+
+    cy.wait("@postNewNaverCard").then(({ status }) => {
+      expect(status).be.equal(200);
+
+      cy.get("[data-cy=modal]");
+      cy.get("[data-cy=go-back]").click();
+      cy.get("[data-cy=go-back-home]").click();
     });
   });
 });
